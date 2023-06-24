4c76513d0f5aef573d1e728d8f1aa2e595a2576f
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-24 16:07:24 +0900

    アプリ名、ロゴ

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <!-- Session Status -->
+    
     <x-auth-session-status class="mb-4" :status="session('status')" />
     管理者用
+    
 
     <form method="POST" action="{{ route('admin.login') }}">
         @csrf
