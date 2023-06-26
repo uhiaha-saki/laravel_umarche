a125969ba6aca85d90696b83fe013d142fa9171f
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-26 19:28:48 +0900

    softDelete

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Owner extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
 
         /**
      * The attributes that are mass assignable.
