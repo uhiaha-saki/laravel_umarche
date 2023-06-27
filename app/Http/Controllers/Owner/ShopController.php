5d59f936e8f1292f45cbb8201752606dc40804c6
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-27 13:36:24 +0900

    middleware,abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Shop;
+use Illuminate\Support\Facades\Auth;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            // dd($request->route()->parameter('shop')); //　文字列
+            // dd(Auth::id()); //数字
+
+            $id= $request->route()->parameter('shop');
+            if(!is_null($id)){
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; //キャスト　文字→数字
+                $ownerId = Auth::id();
+
+                if($shopId !== $ownerId){
+                    abort(404); //404g画面表示
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id',$ownerId)->get();
+
+        return view('owner.shops.index',
+        compact('shops'));
+
+    }
+
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+
+    }
+    
+    public function update(Request $request, $id)
+    {
+
+    }
+}
