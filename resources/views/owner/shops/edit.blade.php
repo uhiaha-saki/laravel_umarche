8b9330285db5f92a8a51da331b076cef37941928
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-27 17:36:07 +0900

    フォームリクエスト

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                   
                     <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id])}}" enctype="multipart/form-data">
                         @csrf
                         <div class="-m-2">
@@ -16,6 +17,7 @@
                                   <div class="relative">
                                     <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                                     <input type="file" id="image" name="image" accept="image/png,image/jpeg,img/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">                                
+                                    <x-input-error :messages="$errors->get('image')" class="mt-2" />
                                   </div>
                               </div>
                               <div class="p-2 w-full flex justify-around mt-4">
@@ -29,3 +31,5 @@
         </div>
     </div>
 </x-app-layout>
+
+{{-- accept="image/png,image/jpeg,img/jpg" --}}
