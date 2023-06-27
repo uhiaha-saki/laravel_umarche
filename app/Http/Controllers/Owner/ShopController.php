d15c4a48cd9cc76bc2537a97dc0d1dcba0e32deb
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-27 19:10:13 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -31,7 +32,6 @@ class ShopController extends Controller
                     abort(404); //404g画面表示
                 }
             }
-
             return $next($request);
         });
     }
@@ -59,13 +59,16 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid()){
-            // Storage::putFile('public/shops',$imageFile); リサイズなしの場合
-            $fileName = uniqid(rand().'_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName. '.' . $extension;
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920,1080)->encode();
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
+
+            // // Storage::putFile('public/shops',$imageFile); リサイズなしの場合
+            // $fileName = uniqid(rand().'_');
+            // $extension = $imageFile->extension();
+            // $fileNameToStore = $fileName. '.' . $extension;
+            // $resizedImage = InterventionImage::make($imageFile)->resize(1920,1080)->encode();
             
-            Storage::put('public/shops/' . $fileNameToStore, $resizedImage );
+            // Storage::put('public/shops/' . $fileNameToStore, $resizedImage );
+            
         }
 
         return redirect()->route('owner.shops.index');
--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -24,7 +24,7 @@ class UploadImageRequest extends FormRequest
     public function rules()
     {
         return [
-            'image'=>'image|mines:jpg,jpeg,png|max:2048',
+            'image'=>'image|mimes:jpg,jpeg,png|max:2048',
         ];
     }
 
