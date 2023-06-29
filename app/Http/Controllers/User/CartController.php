12bd0f1ce4fc321b8dbfc31c6f53f8d4a7f9b20a
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-29 20:34:10 +0900

    cart delete

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -48,7 +48,14 @@ class CartController extends Controller
         
         return redirect()->route('user.cart.index');
 
-        
+    }
 
+    public function delete($id)
+    {
+        Cart::where('product_id', $id)
+        ->where('user_id', Auth::id())
+        ->delete();
+
+        return redirect()->route('user.cart.index');
     }
 }
