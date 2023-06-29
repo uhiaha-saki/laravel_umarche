e27b8562318ec1d4b308ad1795124c07da40ce6e
uhiaha-saki <oneheart.saki0123@gmail.com>
2023-06-30 07:32:07 +0900

    表示順

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -12,4 +12,18 @@ class Common
         'reduce' => self::PRODUCT_REDUCE
 
     ];
+
+    const ORDER_RECOMMEND = '0';
+    const ORDER_HIGHER = '1';
+    const ORDER_LOWER = '2';
+    const ORDER_LATER = '3';
+    const ORDER_OLDER = '4';
+
+    const SORT_ORDER = [
+        'recommend' => self::ORDER_RECOMMEND,
+        'higherPrice' => self::ORDER_HIGHER,
+        'lowerPrice' => self::ORDER_LOWER,
+        'later' => self::ORDER_LATER,
+        'older' => self::ORDER_OLDER
+ ];
 }
